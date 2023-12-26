958022c5bced56747d964e60a0a665f3102e72a7
Blue <adrian.gomezvidal2468@gmail.com>
2023-12-26 15:14:52 +0100

    sgag

--- script1.js
+++ script1.js
@@ -1,11 +1,22 @@
 function checkAnswer(nextPage) {
-    var userAnswer = document.getElementById("reponse").value.toLowerCase();
+    var userAnswer = document.getElementById("reponse").value.toLowerCase().trim();
+    var messageElement = document.getElementById("message"); // Ajout d'un élément pour afficher le message
+
+    // Normalisez la réponse en retirant les articles et en convertissant en minuscules
+    var normalizedAnswer = userAnswer.replace(/^(un |une |le |la |les |)\b/, '').toLowerCase();
 
     // Vérifiez la réponse en fonction de la logique souhaitée
-    if (userAnswer === "piano") {  // Changez cette condition selon votre réponse
+    if (normalizedAnswer === "piano") {
         alert("Félicitations ! Vous avez trouvé la bonne réponse !");
         window.location.href = nextPage;
     } else {
-        alert("Mauvaise réponse. Essayez à nouveau !");
+        // Afficher le message d'erreur en rouge
+        messageElement.style.color = "red";
+        messageElement.textContent = "Mauvaise réponse. Essayez à nouveau !";
+
+        // Vous pouvez également ajouter une logique pour effacer le message après un certain temps si vous le souhaitez
+        setTimeout(function () {
+            messageElement.textContent = "";
+        }, 3000); // Efface le message après 3 secondes (ajustez selon vos besoins)
     }
 }
